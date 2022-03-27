61484396edae13f858c1c9a19d660f22fe19a60a
lucas jose serena <serenalucasjose@gmail.com>
2022-03-27 18:20:59 -0300

    first commit

--- contracts/Zaffe.sol
+++ contracts/Zaffe.sol
@@ -1,31 +1,76 @@
 // contracts/Zafe.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.7.0;
 
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Zafe {
-    struct ServiceAccount { 
+    struct Account {
         string user;
         string password;
     }
 
-    struct User {
-        bool initialized;
-        mapping(string => ServiceAccount) services;
+    mapping(address => uint[]) internal users;
+    Account[] internal accounts;
+    mapping(uint => address) accountToUser;
+
+
+    function createUser() 
+        external 
+    {
+        users[msg.sender] = [];
+        // TODO: Emit Event
     }
 
-    mapping(address => User) internal _users;
+    function destroyUser()
+        external 
+    {
+        delete users[msg.sender];
+        // TODD: Emit Event
+    }
 
+    function createAccount(string calldata _user, string calldata _password)
+        external
+    {
+        Account memory _account = Account(_user, _password);
+        accounts.push(_account);
+        uint accountId = accounts.length - 1;
+        users[msg.sender].push(accountId);
+    }
 
-    function createUser() external {
-        User storage _user = _users[msg.sender];
+    function destroyAccount(uint _accountId) external {
+        require(accountToUser[_accountId] == msg.sender);
 
-        //require(_user.initialized == false, "El usuario ya tiene una cuenta.");
+        accounts.remove(_accountId);
 
-        // User memory newUser = User(true, mapping(
-        //     "facebook": 0
-        // ));
-        // _users[msg.sender] = newUser;
-    } 
+        clearUserAccount(msg.sender, _accountId);
+    }
+    
+    function getAccountsByUser() 
+        public
+        view
+        returns(Account[] memory)
+    {
+        Account[] response = [];
+
+        for (uint256 i = 0; i < users[msg.sender].length; i++)
+        {
+            response.push(accounts[users[msg.sender][i]]);  
+        }
+
+        return response;
+    }
+
+    function clearUserAccount(address _user, uint _accountId)
+        internal
+    {
+        delete accountToUser[_accountId];
+
+        for (uint256 i = 0; i < users[_user].length; i++) {
+            if (users[_user][i] == _accountId)
+            {
+                users[_user].remove(i);
+            }
+        }
+    }
 }
